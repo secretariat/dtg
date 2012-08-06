5e4dfa9015b7a4192eca414e12ef1a4630e44e7e
Secretariat1987 <mixey.viti@bigmir.net>
2012-08-06 13:39:24 -0700

    commit_6

---
+++ app/models/custom.rb
@@ -0,0 +1,3 @@
+class Custom < ActiveRecord::Base
+  attr_accessible :name
+end
---
+++ db/migrate/20120806203323_create_customs.rb
@@ -0,0 +1,9 @@
+class CreateCustoms < ActiveRecord::Migration
+  def change
+    create_table :customs do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
