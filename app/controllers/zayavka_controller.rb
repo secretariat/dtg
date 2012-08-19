bc07501b3c156685bcebb7fc05d75bcc025da0ed
Secretariat1987 <mixey.viti@bigmir.net>
2012-08-19 06:07:29 +0300

    20120819_2

--- app/controllers/zayavka_controller.rb
+++ app/controllers/zayavka_controller.rb
@@ -5,16 +5,17 @@ class ZayavkaController < ApplicationController
 	end
 
 	def create
-		@zayavka = Zayavka.new( params[:zayavka] )
+		# @zayavka = Zayavka.new( params[:zayavka] )
   
-		if @zayavka.save then
-			# DtgMailer.welcome_email( @main, @main.manager, @pass ).deliver
-			flash[:notice] = "Заявка создана успешно"
-			redirect_to( :controller => 'zayavka', :action => 'list')
-		else
-			flash[:notice] = "Ошибка при создании Заявки"
-			redirect_to(:controller => 'manager', :action => 'new')
-		end 
+		# if @zayavka.save then
+		# 	# DtgMailer.welcome_email( @main, @main.manager, @pass ).deliver
+		# 	flash[:notice] = "Заявка создана успешно"
+		# 	redirect_to( :controller => 'zayavka', :action => 'list')
+		# else
+		# 	flash[:notice] = "Ошибка при создании Заявки"
+		# 	redirect_to(:controller => 'user', :action => 'new')
+		# end 
+
 	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120807200449) do
+ActiveRecord::Schema.define(:version => 20120819015807) do
 
   create_table "admins", :force => true do |t|
     t.integer  "main_id"
@@ -63,6 +63,15 @@ ActiveRecord::Schema.define(:version => 20120807200449) do
     t.string   "edrpo"
     t.string   "gdcode"
     t.string   "pib"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "zayavkas", :force => true do |t|
+    t.string   "tamrez"
+    t.string   "typetran"
+    t.string   "cname"
+    t.integer  "garantee"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
