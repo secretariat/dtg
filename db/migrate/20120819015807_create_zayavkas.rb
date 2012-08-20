4d9846b249977b0fa423b80f6e907ab18c610925
Secretariat1987 <mixey.viti@bigmir.net>
2012-08-19 21:01:31 -0700

    db+form

--- db/migrate/20120819015807_create_zayavkas.rb
+++ db/migrate/20120819015807_create_zayavkas.rb
@@ -19,7 +19,7 @@ class CreateZayavkas < ActiveRecord::Migration
       t.integer :field_2_6_4
       t.integer :field_2_7
       t.integer :field_2_8_1
-      t.integer :field_2_8_2 #currency
+      #t.integer :field_2_8_2 #currency
       t.integer :field_3_1_1
       t.integer :field_3_1_2
       t.integer :field_3_2_1
@@ -34,9 +34,9 @@ class CreateZayavkas < ActiveRecord::Migration
       t.string :field_4_3
       t.string :field_4_4
       t.string :field_5_1
-      t.string :field_5_2_1
-      t.string :field_5_2_2
-      t.string :field_5_2_3
+      t.string :field_5_2
+      # t.string :field_5_2_2
+      # t.string :field_5_2_3
       t.string :field_5_3
       t.string :field_6_1
       t.string :field_6_2_1
@@ -50,22 +50,22 @@ class CreateZayavkas < ActiveRecord::Migration
       t.string :field_6_6_3 
       t.string :field_6_7_1 
       t.string :field_6_7_2 
-      t.string :field_6_7_3 
+      # t.string :field_6_7_3 
       # t.string :field_7_1 
       # t.string :field_7_2_1 
       # t.string :field_7_2_2 
       # t.string :field_7_2_3 
       # t.string :field_7_2_4
       t.string :field_8_1 #ekspeditori
-      t.string :field_8_2_1
-      t.string :field_8_2_2
-      t.string :field_8_2_3
+      t.string :field_8_2
+      # t.string :field_8_2_2
+      # t.string :field_8_2_3
       t.string :field_8_3
       t.string :field_8_4
       t.string :field_9_1 #perevozshik
-      t.string :field_9_2_1
-      t.string :field_9_2_2
-      t.string :field_9_2_3
+      t.string :field_9_2
+      # t.string :field_9_2_2
+      # t.string :field_9_2_3
       t.string :field_9_3
       t.string :field_9_4_1
       t.string :field_9_4_2
