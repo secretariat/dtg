9b09af3c50ca63e57260c84439ee276012d44fa3
Secretariat1987 <mixey.viti@bigmir.net>
2012-10-10 23:27:55 +0300

    20121010_copy_new

--- app/controllers/zayavka_controller.rb
+++ app/controllers/zayavka_controller.rb
@@ -6,12 +6,20 @@ class ZayavkaController < ApplicationController
 	before_filter :get_uniq_user 
 
 	def copy
-		@zayavka = Zayavka.new
-		# @zayavka = Zayavka.find( params[:id] )
-		# @z = @zayavka
-		@zayavka.save
+		@zayavka = Zayavka.new( Zayavka.find( params[:id] ).attributes )
+		@ttt = Zayavka.last
+		if( @ttt == nil ) then
+			@zid = 1
+		else
+			@zid = @ttt.id+1
+		end
+
+		1.times do
+			@zayavka.products.build
+		end
 		@user = User.find( session[:uid] )
-		render 'edit'
+		
+		render 'new'
 	end
 
 	def list	
