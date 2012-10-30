61996c302a165813df3392ae556135a3cff08c5d
Secretariat1987 <mixey.viti@bigmir.net>
2012-10-30 22:02:49 +0200

    20121030_rc05

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,9 +1,9 @@
 # -*- encoding : utf-8 -*-
 class Manager < ActiveRecord::Base
 	belongs_to :main
-  attr_accessible :email, :phone, :pib, :postion
+  attr_accessible :email, :phone, :pib, :position
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => ": непрваильный формат email"
   validates_presence_of :pib, :message => ": Поле ФИО должно быть заполнено"
   validates_presence_of :phone, :message => ": Поле Телефон должно быть заполнено"
-  validates_presence_of :postion, :message => ": Поле Должность должно быть заполнено"
+  validates_presence_of :position, :message => ": Поле Должность должно быть заполнено"
 end
